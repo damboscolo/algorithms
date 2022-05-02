eacb2eedd85ec238e8e9a178359c4c43876c621f
Daniele Boscolo <daniele.boscolo@rappi.com>
2022-05-02 10:16:56 -0300

    can construct

---
+++ src/main/java/easy/CanConstruct.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.HashMap;
+
+public class CanConstruct {
+
+    public boolean recursive(String target, String[] wordBank) {
+        if (target.equals("")) return true;
+
+        for (String word : wordBank) {
+            if (target.indexOf(word) == 0) { // if word is the prefix
+                String suffix = target.replaceFirst(word, "");
+                if (recursive(suffix, wordBank)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean memoized(String target, String[] wordBank) {
+        return memoized(target, wordBank, new HashMap());
+    }
+    private boolean memoized(String target, String[] wordBank, HashMap<String, Boolean> memo) {
+        if (memo.containsKey(target)) return memo.get(target);
+        if (target.equals("")) return true;
+
+        for (String word : wordBank) {
+            if (target.indexOf(word) == 0) {
+                String suffix = target.replaceFirst(word, "");
+                if (memoized(suffix, wordBank, memo)) {
+                    memo.put(target, true);
+                    return true;
+                }
+            }
+        }
+        memo.put(target, false);
+        return false;
+    }
+}
---
+++ src/test/java/easy/CanConstructTest.java
@@ -0,0 +1,29 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CanConstructTest {
+
+    CanConstruct t = new CanConstruct();
+
+    @Test
+    public void testRecursive() {
+        assertEquals(true, t.recursive("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd"} ));
+        assertEquals(false, t.recursive("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"} ));
+
+        // Above will take too long to run
+        //assertEquals(false, t.recursive("eeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] {"e", "eee", "eeeee", "ee"} ));
+    }
+
+    @Test
+    public void testMemoized() {
+        assertEquals(true, t.memoized("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd"} ));
+        assertEquals(false, t.memoized("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"} ));
+
+        // Runs fast
+        assertEquals(false, t.memoized("eeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] {"e", "eee", "eeeee", "eeeeeeee", "eeeee"} ));
+    }
+
+}
