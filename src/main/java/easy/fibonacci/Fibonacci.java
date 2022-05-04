768e86fcaf7161dd498f4df9a19a0e52f9af1aa8
Daniele Boscolo <daniele.boscolo@rappi.com>
2022-05-04 11:35:28 -0300

    fibonacci tabled

--- src/main/java/easy/fibonacci/Fibonacci.java
+++ src/main/java/easy/fibonacci/Fibonacci.java
@@ -13,18 +13,13 @@ Given n, calculate F(n) -> it means, given n, calculate the number on the index 
  */
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Fibonacci {
-    // time: O(2^n) space: O(n)
-    public int fibRecursion(int n) {
-        if (n <= 1) return n;
-        return fibRecursion(n - 1) + fibRecursion(n - 2);
-    }
-
     // time: O(n) space: O(1)
-    public int fibNotRecursive(int n) {
+    public int common(int n) {
         if (n <= 1) return n;
         int a = 0, b = 1;
 
@@ -35,6 +30,58 @@ public class Fibonacci {
         }
         return b;
     }
+
+    // time: O(2^n) space: O(n)
+    public int recursive(int n) {
+        if (n <= 1) return n;
+        return recursive(n - 1) + recursive(n - 2);
+    }
+
+    /* After studied Dynamic Programming
+        YouTube: https://www.youtube.com/watch?v=oBt53YbR9Kk&ab_channel=freeCodeCamp.org
+        Notion: https://www.notion.so/Dynamic-Programming-32f1c4d15bfd4089aea59a8d7712e362
+     */
+    public Long memoized(int n) {
+        return memoized(n, new HashMap());
+    }
+    public Long memoized(int n, Map<Integer, Long> memo) {
+        if (memo.containsKey(n)) return memo.get(n);
+        if (n < 1) return 0L;
+        if (n <= 2) return 1L;
+        memo.put(n, memoized(n - 1, memo) + memoized(n - 2, memo));
+        return memo.get(n);
+    }
+
+    public BigInteger memoizedBigInteger(int n) {
+        return memoizedBigInteger(n, new HashMap());
+    }
+    public BigInteger memoizedBigInteger(int n, Map<Integer, BigInteger> memo) {
+        if (memo.containsKey(n)) return memo.get(n);
+        if (n == 0) return BigInteger.valueOf(0);
+        if (n <= 2) return BigInteger.valueOf(1);
+        BigInteger result = memoizedBigInteger(n-1, memo).add(memoizedBigInteger(n-2, memo));
+        memo.put(n, result);
+        return result;
+    }
+
+    // Tabled
+    public long tabledLong(int n) {
+        long[] table = new long[n + 2]; // +2 = to not raise out of bound error when calculate the next 2 numbers
+        table[1] = 1; // start point
+        for (int i = 0; i < n; i++) {
+            table[i + 1] += table[i];
+            table[i + 2] += table[i];
+        }
+        return table[n];
+    }
+
+    // TODO:
+//    public BigInteger tabledBigInteger(int n) {
+//        ArrayList<BigInteger> table = new ArrayList<BigInteger>();
+//        while (table.size() <= n) table.add(BigInteger.valueOf(0L)); // fill with zeros
+//        return table.get(n);
+//    }
+
 
     // discussion
     // https://leetcode.com/problems/fibonacci-number/discuss/215992/Java-Solutions/1219401/
@@ -50,33 +97,6 @@ public class Fibonacci {
             return fib_cache[N];
         else
             return fib_cache[N] = fibDynamicProgrammingTopDown(N - 1) + fibDynamicProgrammingTopDown(N - 2);
-    }
-
-    /* After studied Dynamic Programming
-        YouTube: https://www.youtube.com/watch?v=oBt53YbR9Kk&ab_channel=freeCodeCamp.org
-        Notion: https://www.notion.so/Dynamic-Programming-32f1c4d15bfd4089aea59a8d7712e362
-     */
-    public Long fibDynamicProgrammingStudy(int n) {
-        return fibDynamicProgrammingStudy(n, new HashMap());
-    }
-    public Long fibDynamicProgrammingStudy(int n, Map<Integer, Long> memo) {
-        if (memo.containsKey(n)) return memo.get(n);
-        if (n < 1) return 0L;
-        if (n <= 2) return 1L;
-        memo.put(n, fibDynamicProgrammingStudy(n - 1, memo) + fibDynamicProgrammingStudy(n - 2, memo));
-        return memo.get(n);
-    }
-
-    public BigInteger fibBigInteger(int n) {
-        return fibBigInteger(n, new HashMap());
-    }
-    public BigInteger fibBigInteger(int n, Map<Integer, BigInteger> memo) {
-        if (memo.containsKey(n)) return memo.get(n);
-        if (n == 0) return BigInteger.valueOf(0);
-        if (n <= 2) return BigInteger.valueOf(1);
-        BigInteger result = fibBigInteger(n-1, memo).add(fibBigInteger(n-2, memo));
-        memo.put(n, result);
-        return result;
     }
 
     // time O(n) space O(n)
@@ -95,10 +115,3 @@ public class Fibonacci {
         return fib_cache[N];
     }
 }
-
-// 0 = 0
-// 1 = 1
-// 2 = 1 + 0 = 1
-// 3 = 2 + 1 = 3
-// 4 = f(3) + f(2) = 2 + 1 = 3
-// 5 = 4 + 3 = 7
--- src/test/java/easy/FibonacciTest.java
+++ src/test/java/easy/FibonacciTest.java
@@ -14,35 +14,101 @@ class FibonacciTest {
 
     @Test
     @DisplayName("Given a number then return the fibonacci sum")
-    public void testFibRecursion() {
-        assertEquals(0, f.fibRecursion(0));
-        assertEquals(1, f.fibRecursion(1));
-        assertEquals(1, f.fibRecursion(2));
-        assertEquals(2, f.fibRecursion(3));
-        assertEquals(3, f.fibRecursion(4));
-        assertEquals(8, f.fibRecursion(6));
-        assertEquals(13, f.fibRecursion(7));
-        assertEquals(21, f.fibRecursion(8));
-        assertEquals(34, f.fibRecursion(9));
-        assertEquals(55, f.fibRecursion(10));
-        assertEquals(6765, f.fibRecursion(20));
+    public void fibCommon() {
+        assertEquals(0, f.common(0));
+        assertEquals(1, f.common(1));
+        assertEquals(1, f.common(2));
+        assertEquals(2, f.common(3));
+        assertEquals(3, f.common(4));
+        assertEquals(8, f.common(6));
+        assertEquals(13, f.common(7));
+        assertEquals(21, f.common(8));
+        assertEquals(34, f.common(9));
+        assertEquals(55, f.common(10));
+        assertEquals(6765, f.common(20));
+    }
+
+    @Test
+    @DisplayName("Given a number then return the fibonacci sum")
+    public void testRecursive() {
+        assertEquals(0, f.recursive(0));
+        assertEquals(1, f.recursive(1));
+        assertEquals(1, f.recursive(2));
+        assertEquals(2, f.recursive(3));
+        assertEquals(3, f.recursive(4));
+        assertEquals(8, f.recursive(6));
+        assertEquals(13, f.recursive(7));
+        assertEquals(21, f.recursive(8));
+        assertEquals(34, f.recursive(9));
+        assertEquals(55, f.recursive(10));
+        assertEquals(6765, f.recursive(20));
         // More tha 20 it takes too much time because of the O(2^2)
     }
 
     @Test
     @DisplayName("Given a number then return the fibonacci sum")
-    public void fibNotRecursive() {
-        assertEquals(0, f.fibNotRecursive(0));
-        assertEquals(1, f.fibNotRecursive(1));
-        assertEquals(1, f.fibNotRecursive(2));
-        assertEquals(2, f.fibNotRecursive(3));
-        assertEquals(3, f.fibNotRecursive(4));
-        assertEquals(8, f.fibNotRecursive(6));
-        assertEquals(13, f.fibNotRecursive(7));
-        assertEquals(21, f.fibNotRecursive(8));
-        assertEquals(34, f.fibNotRecursive(9));
-        assertEquals(55, f.fibNotRecursive(10));
-        assertEquals(6765, f.fibNotRecursive(20));
+    public void tesMemoized() {
+        assertEquals(0, f.memoized(0));
+        assertEquals(1, f.memoized(1));
+        assertEquals(1, f.memoized(2));
+        assertEquals(2, f.memoized(3));
+        assertEquals(3, f.memoized(4));
+        assertEquals(8, f.memoized(6));
+        assertEquals(13, f.memoized(7));
+        assertEquals(21, f.memoized(8));
+        assertEquals(34, f.memoized(9));
+        assertEquals(55, f.memoized(10));
+        assertEquals(6765, f.memoized(20));
+        // here we can calculate much more values because of the memo
+        assertEquals(2971215073L, f.memoized(47));
+        assertEquals(12586269025L, f.memoized(50));
+
+        // PS: Long/long can't handle the number 31940434634990099905 because it is too large. Need to use BigInteger
+        // assertEquals(31940434634990099905L, f.fibDynamicProgrammingStudy(95));
+    }
+
+    @Test
+    @DisplayName("Given a number then return the fibonacci sum")
+    public void testMemoizedBigInteger() {
+        assertEquals(BigInteger.valueOf(0), f.memoizedBigInteger(0));
+        assertEquals(BigInteger.valueOf(1), f.memoizedBigInteger(1));
+        assertEquals(BigInteger.valueOf(1), f.memoizedBigInteger(2));
+        assertEquals(BigInteger.valueOf(2), f.memoizedBigInteger(3));
+        assertEquals(BigInteger.valueOf(3), f.memoizedBigInteger(4));
+        assertEquals(BigInteger.valueOf(8), f.memoizedBigInteger(6));
+        assertEquals(BigInteger.valueOf(13), f.memoizedBigInteger(7));
+        assertEquals(BigInteger.valueOf(21), f.memoizedBigInteger(8));
+        assertEquals(BigInteger.valueOf(34), f.memoizedBigInteger(9));
+        assertEquals(BigInteger.valueOf(55), f.memoizedBigInteger(10));
+        assertEquals(BigInteger.valueOf(6765), f.memoizedBigInteger(20));
+        // here we can calculate much more values because of the memo
+        assertEquals(BigInteger.valueOf(2971215073L), f.memoizedBigInteger(47));
+        assertEquals(new BigInteger("12586269025"), f.memoizedBigInteger(50));
+
+        // BigInteger can handle the number 31940434634990099905!
+        assertEquals(new BigInteger("31940434634990099905"), f.memoizedBigInteger(95));
+    }
+
+    @Test
+    @DisplayName("Given a number then return the fibonacci sum")
+    public void testTabledInt() {
+        assertEquals(0, f.tabledLong(0));
+        assertEquals(1, f.tabledLong(1));
+        assertEquals(1, f.tabledLong(2));
+        assertEquals(2, f.tabledLong(3));
+        assertEquals(3, f.tabledLong(4));
+        assertEquals(8, f.tabledLong(6));
+        assertEquals(13, f.tabledLong(7));
+        assertEquals(21, f.tabledLong(8));
+        assertEquals(34, f.tabledLong(9));
+        assertEquals(55, f.tabledLong(10));
+        assertEquals(6765, f.tabledLong(20));
+//        // here we can calculate much more values because of the memo
+        assertEquals(2971215073L, f.tabledLong(47));
+        assertEquals(12586269025L, f.tabledLong(50));
+
+        // PS: Long/long can't handle the number 31940434634990099905 because it is too large. Need to use BigInteger
+        // assertEquals(31940434634990099905L, f.fibDynamicProgrammingStudy(95));
     }
 
     @Test
@@ -59,49 +125,6 @@ class FibonacciTest {
         assertEquals(34, f.fibDynamicProgrammingTopDown(9));
         assertEquals(55, f.fibDynamicProgrammingTopDown(10));
         assertEquals(6765, f.fibDynamicProgrammingTopDown(20));
-    }
-
-    @Test
-    @DisplayName("Given a number then return the fibonacci sum")
-    public void tesFibDynamicProgrammingStudy() {
-        assertEquals(0, f.fibDynamicProgrammingStudy(0));
-        assertEquals(1, f.fibDynamicProgrammingStudy(1));
-        assertEquals(1, f.fibDynamicProgrammingStudy(2));
-        assertEquals(2, f.fibDynamicProgrammingStudy(3));
-        assertEquals(3, f.fibDynamicProgrammingStudy(4));
-        assertEquals(8, f.fibDynamicProgrammingStudy(6));
-        assertEquals(13, f.fibDynamicProgrammingStudy(7));
-        assertEquals(21, f.fibDynamicProgrammingStudy(8));
-        assertEquals(34, f.fibDynamicProgrammingStudy(9));
-        assertEquals(55, f.fibDynamicProgrammingStudy(10));
-        assertEquals(6765, f.fibDynamicProgrammingStudy(20));
-        // here we can calculate much more values because of the memo
-        assertEquals(2971215073L, f.fibDynamicProgrammingStudy(47));
-        assertEquals(12586269025L, f.fibDynamicProgrammingStudy(50));
-
-        // PS: Long/long can't handle the number 31940434634990099905 because it is too large. Need to use BigInteger
-//        assertEquals(31940434634990099905L, f.fibDynamicProgrammingStudy(95));
-    }
-    @Test
-    @DisplayName("Given a number then return the fibonacci sum")
-    public void tesFibBigInteger() {
-        assertEquals(BigInteger.valueOf(0), f.fibBigInteger(0));
-        assertEquals(BigInteger.valueOf(1), f.fibBigInteger(1));
-        assertEquals(BigInteger.valueOf(1), f.fibBigInteger(2));
-        assertEquals(BigInteger.valueOf(2), f.fibBigInteger(3));
-        assertEquals(BigInteger.valueOf(3), f.fibBigInteger(4));
-        assertEquals(BigInteger.valueOf(8), f.fibBigInteger(6));
-        assertEquals(BigInteger.valueOf(13), f.fibBigInteger(7));
-        assertEquals(BigInteger.valueOf(21), f.fibBigInteger(8));
-        assertEquals(BigInteger.valueOf(34), f.fibBigInteger(9));
-        assertEquals(BigInteger.valueOf(55), f.fibBigInteger(10));
-        assertEquals(BigInteger.valueOf(6765), f.fibBigInteger(20));
-        // here we can calculate much more values because of the memo
-        assertEquals(BigInteger.valueOf(2971215073L), f.fibBigInteger(47));
-        assertEquals(new BigInteger("12586269025"), f.fibBigInteger(50));
-
-        // BigInteger can handle the number 31940434634990099905!
-        assertEquals(new BigInteger("31940434634990099905"), f.fibBigInteger(95));
     }
 
     @Test
